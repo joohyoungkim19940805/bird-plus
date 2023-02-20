4d7f6e1e1523cecd61450ef9102d258bfab97364
joohyoungkim19940805 <oozu1994@gmail.com>
2023-02-20 22:07:20 +0900

    불필요 패키지 제거

--- bird-plus/src/main/java/com/radcns/bird_plus/repository/AccountLogRepository.java
+++ bird-plus/src/main/java/com/radcns/bird_plus/repository/AccountLogRepository.java
@@ -1,8 +1,6 @@
 package com.radcns.bird_plus.repository;
 
-import java.time.LocalDateTime;
 
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import com.radcns.bird_plus.entity.AccountLogEntity;
